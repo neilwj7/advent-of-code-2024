f1588d3498d96c573022539bf366fe2648ca8370
neil <neilwj07@gmail.com>
2024-12-27 20:56:36 -0800

    completed day 16

--- 12-16-2024/solution.py
+++ 12-16-2024/solution.py
@@ -1,15 +1,13 @@
 from math import inf
 from collections import deque
-from copy import deepcopy
+import heapq
 
 class solution:
     def __init__(self, inputFile):
         self.grid = self.getInput(inputFile)
         self.start, self.end = self.findEndpoints()
-        self.endSet = set()
-        self.endScore = inf
-        self.Opt = [[inf for j in range(len(self.grid[0]))] for i in range(len(self.grid))]
-        self.bfs()
+        self.opt = {}
+        self.dijkstra()
 
     def getInput(self, fileName):
         res = []
@@ -27,64 +25,57 @@ class solution:
                 if self.grid[i][j] == 'E':
                     res[1] = (i, j)
         return res
-    
-    def myPrint(self, s):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                if (i, j) in s:
-                    print('*', end='')
-                else:
-                    print(' ', end='')
-            print()
+
+    def dijkstra(self):
+        h = []      # (x, y, dir, score)
+        seen = set()
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+        heapq.heappush(h, (0, self.start[0], self.start[1], 1))
+
+        while h:
+            score, x, y, dir = heapq.heappop(h)
+            self.opt[(x, y, dir)] = score
+            seen.add((x, y, dir))
+            dx, dy = dirs[dir]
+            if (x + dx, y + dy, dir) not in seen and self.grid[x + dx][y + dy] != '#':
+                heapq.heappush(h, (score + 1, x + dx, y + dy, dir))
+            if (x, y, (dir + 1) % 4) not in seen: heapq.heappush(h, (score + 1000, x, y, (dir + 1) % 4))
+            if (x, y, (dir - 1) % 4) not in seen: heapq.heappush(h, (score + 1000, x, y, (dir - 1) % 4))
 
     def solveP1(self):
-        return self.endScore
-
-    def solveP2(self):
-        return len(self.endSet)
+        return min([self.opt[(self.end[0], self.end[1], i)] for i in range(4)])
     
-    def bfs(self):
-        q = deque()     # (i, j, dir, score, setOfPath)
-        #self.responsibleFor[(self.start[0], self.start[1])].add((self.start[0], self.start[1]))
-        startingset = set()
-        q.append((self.start[0], self.start[1], 1, 0))
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        while len(q) != 0:
-            i, j, dir, score = q.popleft()
-            #print(f"({i}, {j})")
-            # if (i, j) in s:
-            #     continue
-            # if self.Opt[i][j] + 1000 < score or self.endScore < score:
-            #     continue
+    def solveP2(self):
+        inPath = [[False for i in range(len(self.grid[0]))] for j in range(len(self.grid))]
+        seen = set()
+        dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        q = deque()
+        for dir in range(4):
+            if self.opt[(self.end[0], self.end[1], dir)] == min([self.opt[(self.end[0], self.end[1], i)] for i in range(4)]):
+                q.append((self.end[0], self.end[1], dir, min([self.opt[(self.end[0], self.end[1], i)] for i in range(4)])))
+                seen.add((self.end[0], self.end[1], dir))
 
-            self.Opt[i][j] = min(score, self.Opt[i][j])
-            #s.add((i, j))
+        # backtracking bfs
+        while q:
+            x, y, dir, score = q.popleft()
+            inPath[x][y] = True
+            dx, dy = dirs[dir]
+            if (x - dx, y - dy, dir) not in seen and self.opt.get((x - dx, y - dy, dir), inf) == score - 1:
+                seen.add((x - dx, y - dy, dir))
+                q.append((x - dx, y - dy, dir, score - 1))
+            if (x, y, (dir + 1) % 4) not in seen and self.opt.get((x, y, (dir + 1) % 4), inf) == score - 1000:
+                seen.add((x, y, (dir + 1) % 4))
+                q.append((x, y, (dir + 1) % 4, score - 1000))
+            if (x, y, (dir - 1) % 4) not in seen and self.opt.get((x, y, (dir - 1) % 4), inf) == score - 1000:
+                seen.add((x, y, (dir - 1) % 4))
+                q.append((x, y, (dir - 1) % 4, score - 1000))
+        
+        # count positions in path
+        res = 0
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                if inPath[i][j]:
+                    res += 1
+        return res
 
-            if (i, j) == (self.end[0], self.end[1]):
-                if score < self.endScore:
-                    #print(f"new end score: {score}")
-                    self.endScore = score
-                    #self.endSet = s
-                elif (score == self.endScore):
-                    #self.endSet = self.endSet | s
-                    pass
-                continue
-
-            # same direction
-            dy, dx = directions[dir]
-            if self.grid[i + dy][j + dx] != '#':
-                #newS = deepcopy(s)
-                q.append((i + dy, j + dx, dir, score + 1))
-            
-            # one up
-            dy, dx = directions[(dir + 1) % 4]
-            if self.grid[i + dy][j + dx] != '#':
-                #newS = deepcopy(s)
-                q.append((i + dy, j + dx, (dir + 1) % 4, score + 1001))
-
-            # one down
-            dy, dx = directions[(dir - 1) % 4]
-            if self.grid[i + dy][j + dx] != '#':
-                #newS = deepcopy(s)
-                q.append((i + dy, j + dx, (dir - 1) % 4, score + 1001))
-        return 
